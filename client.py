6acde18304d5d7bb226086a494c01c505af7e939
Ankit Gohel <ankitgohel1996@gmail.com>
2018-09-08 14:00:58 -0500

    Client almost works

--- client.py
+++ client.py
@@ -1,23 +1,40 @@
 import threading
 import json
 import socket
+import Queue
 
 class QueryThread(threading.Thread):
 
-	def __init__(self, command, host, port):
-		self.command = command
+	def __init__(self, command, host, port, logfile_name, queue):
+		super(QueryThread, self).__init__()
+		self.command = command + ' ' + logfile_name
 		self.host = host
 		self.port = port
+		self.queue = queue
+		self.result = ''
+		self.logfile_name = logfile_name
 
 	def run(self):
 		try:
 			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			sock.connect((self.host, self.port))
-			sock.sendall(self.command)
-			
-		except socket.error as e:
-			pass
+			sock.connect((self.host, int(self.port)))
+		except socket.error as e: #If connection to remote machine fails
+			print 'Could not connect'
+			return
 
+		try:
+			sock.sendall(self.command)
+
+			while True:
+				part = sock.recv(1024)
+				if part:
+					self.result += part
+				else:
+					self.queue.put({self.logfile_name: self.result})
+					break
+
+		except socket.error as e:
+			return
 
 class Client(object):
 
@@ -27,21 +44,30 @@ class Client(object):
 			config_data = json.load(config)
 		self.servers = config_data['servers']
 		self.port = config_data['port']
+		self.queue = Queue.Queue()
 
 	def query(self):
 		thread_list = []
 
 		for server in self.servers:
 			host, ip = server['host'], server['ip']
-			thread = QueryThread(self.command, self.ip, self.port)
+			thread = QueryThread(self.command, ip, self.port, server['logfile_name'], self.queue)
 			thread.start()
 			thread_list.append(thread)
 
 		for thread in thread_list:
 			thread.join()
 
+		while not self.queue.empty():
+			item = self.queue.get()
+			file_name, result = item.keys()[0], item.values()[0]
+
+			for line in result.split('\n'):
+				print 'Filename:%s, Line %s ' % (file_name, line)
+
 def main():
-	Client('test')
+	obj = Client('grep -n "import"')
+	obj.query()
 
 if __name__ == '__main__':
 	main()
