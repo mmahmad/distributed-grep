134253f26f081f1abe3873b7ea700d9d1580338a
Ankit Gohel <ankitgohel1996@gmail.com>
2018-09-08 19:12:38 -0500

    Added test method stubs

--- test.py
+++ test.py
@@ -2,12 +2,27 @@ import unittest
 from client import Client
 
 class TestLogQuerier(unittest.TestCase):
-	 def test_two_files(self):
-	 	command = "grep -n 'self.host = host'"
-	 	obj = Client(command)
+	def test_rare_patterns(self):
+		pass
 
-	 	expected_output = 'Filename:client.py, Line 11:\t\tself.host = host '
-	 	self.assertEqual(expected_output, obj.query())
+	def test_somewhat_frequent_patterns(self):
+		pass
+
+	def test_frequent_patterns(self):
+		pass
+
+	def test_does_not_occur(self):
+		pass
+
+	def test_occur_in_one_log(self):
+		pass
+
+	def test_occur_in_some_logs(self):
+		pass
+
+	def test_occur_in_all_logs(self):
+		pass
 
 if __name__ == '__main__':
-    unittest.main()
+	suite = unittest.TestLoader().loadTestsFromTestCase(TestLogQuerier)
+	unittest.TextTestRunner(verbosity=2).run(suite)
